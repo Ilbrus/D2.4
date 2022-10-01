c1d1e11355a29ad1c070f9babc0c189cee05a1c4
Iliya <6262140@gmail.com>
2022-10-01 19:49:21 +0300

    Add final

--- NewsPaper/sign/forms.py
+++ NewsPaper/sign/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Post
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
 
 class UpdateProfileForm(ModelForm):
     class Meta:
@@ -10,3 +12,11 @@ class UpdateProfileForm(ModelForm):
             'first_name',
             'last_name',
         ]
+        
+class BasicSignupForm(SignupForm):
+    
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='authors')
+        basic_group.user_set.add(user)
+        return user
