d18e87ea2e563c7ef136eb0e9b1112cc2ef3814b
Iliya <6262140@gmail.com>
2022-10-06 21:33:26 +0300

    Add 6.5.4(2)

---
+++ NewsPaper/news/signals.py
@@ -0,0 +1,37 @@
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+from .models import Post
+
+
+def send_notification(preview, pk, title, subscribers):
+    html_context = render_to_string(
+        'post_created_email.thml',
+        {
+            'text': preview,
+            'link': f'{setings.SITE_URL}/news/{pk}'
+        }        
+        )
+    
+    msg = EmailMultiAlternatives(
+        subjects=title,
+        body='',
+        from_email=setings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+    
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+@receiver(m2m_changed, sender=Post.postCategory)
+def notify_abiut_new_post(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        categories = instance.category.all()
+        subscribers: list[str] = []
+        for category in categories:
+            subscribers += category.subscribers.all()
+            
+        subscribers = [s.mail for s in subscribers]
+        
+        send_notification(instance.preview(), instance.pk, instance.title, subscribers)
